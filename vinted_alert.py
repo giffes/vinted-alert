21d4c13814e05c3a2c43e23cc30b77cead466864
Giulia F <62563641+giffes@users.noreply.github.com>
2026-05-29 07:37:15 -0300

    Atualizar o vinted_alert.py

--- vinted_alert.py
+++ vinted_alert.py
@@ -9,10 +9,11 @@ CHAT_ID = os.environ["CHAT_ID"]
 URL = "https://www.vinted.pt/catalog?catalog[]=11&brand_ids[]=377&brand_ids[]=567&brand_ids[]=671&brand_ids[]=1745&brand_ids[]=2113&brand_ids[]=3573&brand_ids[]=4559&brand_ids[]=10613&brand_ids[]=14217&brand_ids[]=83122&brand_ids[]=7011975&brand_ids[]=15430438&brand_ids[]=51445&brand_ids[]=56974&brand_ids[]=200474&brand_ids[]=72138&order=newest_first&currency=EUR&page=1"
 
 headers = {
-    "User-Agent": "Mozilla/5.0"
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136 Safari/537.36"
 }
 
 r = requests.get(URL, headers=headers)
+
 html = r.text
 
 matches = re.findall(r'\/items\/([^\"]+)', html)
@@ -27,7 +28,7 @@ if os.path.exists(STATE_FILE):
     with open(STATE_FILE, "r") as f:
         previous = json.load(f)
 
-new_items = [i for i in items if i not in previous]
+new_items = [item for item in items if item not in previous]
 
 for item in reversed(new_items):
 
